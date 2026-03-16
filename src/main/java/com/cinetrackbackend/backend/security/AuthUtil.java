61bcdc5bd18ca30e37b6a8518d494e9906ca1e4e
yash-singh-45 <yashrajput2022t@gmail.com>
2026-03-17 01:53:41 +0530

    Update AuthUtil.java

--- src/main/java/com/cinetrackbackend/backend/security/AuthUtil.java
+++ src/main/java/com/cinetrackbackend/backend/security/AuthUtil.java
@@ -28,7 +28,7 @@ public class AuthUtil {
         .subject(user.getUsername())
         .claim("userId", user.getId().toString())
         .issuedAt(new Date())
-        .expiration(new Date(System.currentTimeMillis() + 1000*60*10))
+        .expiration(new Date(System.currentTimeMillis() + 1000 * 60 * 60 * 24 * 3))
         .signWith(getSecretKey())
         .compact();
     }
